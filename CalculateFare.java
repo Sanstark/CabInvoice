4ce8eee2409a741051403bff639873678b728b16
Sanstark <47468504+Sanstark@users.noreply.github.com>
2020-11-02 23:21:35 +0530

    Add files via upload

---
+++ CalculateFare.java
@@ -0,0 +1,18 @@
+public class CalculateFare{
+    private static final double COST_PERKM = 10.0;
+    private static final int COST_PERUNITTIME = 1;
+
+    public double getTotalFare(double dist, int time) {
+        if(dist < 0 || time < 0)
+            return -1;
+        return (dist * COST_PERKM + time * COST_PERUNITTIME) < 5 ? 5 : dist * COST_PERKM + time * COST_PERUNITTIME;
+    }
+
+    public double getTotalFare(Ride[] ride) {
+        double sum = 0;
+        for(Ride r : ride){
+            sum = sum + this.getTotalFare(r.getDist(), r.getTime());
+        }
+        return sum;
+    }
+}
